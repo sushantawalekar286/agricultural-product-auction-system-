da49ceb2a57d2256c7d1a4041da67d5d304490d5
Sushant Awalekar <sushantawalekar286@gmail.com>
2026-03-30 09:52:02 +0530

    Delete agribid directory

--- agribid/server/routes/serviceRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { predictCropDemand } from '../services/predictionService.js';
-import { protect } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.get('/predict', protect, predictCropDemand);
-
-export default router;
